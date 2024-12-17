3892c69c2890891e9af006d77076836dc3456ba7
wtdavis <wtdavis@wesleyan.edu>
2024-12-16 21:33:22 -0500

    leet code pracitce

---
+++ leet0.js
@@ -0,0 +1,10 @@
+function reverseNum (num) {
+    
+}
+
+console.log(`the result of 121 is ${reverseNum(121)}`)
+console.log(`the result of 123 is ${reverseNum(321)}`)
+console.log(`the result of 100 is ${reverseNum(100)}`)
+console.log(`the result of 4 is ${reverseNum(4)}`)
+
+
