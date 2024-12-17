712894b026502ce375f18fbcf5b2c21d960a5264
wtdavis <wtdavis@wesleyan.edu>
2024-12-16 21:41:21 -0500

    recursive function practice

--- leet0.js
+++ leet0.js
@@ -1,5 +1,13 @@
 function reverseNum (num) {
-    
+    //we want to remove digits in num from the 1's place, and store them in the order we take them, which will reverse the order of the digits when they are recombined
+
+    //recursive function to make digits in num ennumerable
+    function ennumerable (input) {
+        if (input % 10 === input ){
+        return input}
+
+        
+    }
 }
 
 console.log(`the result of 121 is ${reverseNum(121)}`)
