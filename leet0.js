1902efd1f68ec162452986a88829ac8ce3fc6f8f
wtdavis <wtdavis@wesleyan.edu>
2024-12-16 22:15:19 -0500

    nailed it

--- leet0.js
+++ leet0.js
@@ -3,20 +3,27 @@ function reverseNum (num) {
 
     //recursive function to make digits in num ennumerable
     function ennumerable (input) {
-        //if there is only 1 digit, return digit
+            //if there is only 1 digit, return digit
         if (input % 10 === input ){
-        return input}
+        return [input]}
             // grab the 1's digit with modulo, then get whats left without it, using division
         let digit = input % 10
-        let divided = input / 10
+        let divided = Math.floor(input / 10)
 
-
+       let res = ennumerable(divided)
+       let returnArr = []
+       returnArr.push(digit)
+       returnArr = returnArr.concat(res)
+       console.log(returnArr)
+       return(returnArr)
     }
+
+    return ennumerable(num)
 }
 
+console.log(`the result of 4 is ${reverseNum(4)}`)
 console.log(`the result of 121 is ${reverseNum(121)}`)
 console.log(`the result of 123 is ${reverseNum(321)}`)
 console.log(`the result of 100 is ${reverseNum(100)}`)
-console.log(`the result of 4 is ${reverseNum(4)}`)
 
 
